5d317c6d7f06496c9df18f9bc22a049bd259fe44
Dustin LaDue <dustin.ladue@yahoo.com>
2020-04-14 10:56:24 -0700

    Button functionality in local storage

--- src/idea.js
+++ src/idea.js
@@ -1,30 +1,31 @@
 class Idea {
-  constructor(title, body) {
-    this.id = Date.now;
+  constructor(title, body, star, id) {
     this.title = title;
     this.body = body;
-    this.star = false;
+    this.star = star || false;
+    this.id = id || Date.now();
   }
-
-  saveToStorage() {
+​
+  saveToStorage(savedCards) {
+    localStorage.setItem("savedCardsToStorage", JSON.stringify(savedCards));
   }
-
-  deleteFromStorage() {
+​
+  deleteFromStorage(id) {
+    var savedCardsFromStorage = JSON.parse(localStorage.getItem("savedCardsToStorage"));
+    var filteredCards = savedCardsFromStorage.filter(function(idea) {
+      return idea.id != id;
+    } )
+    localStorage.setItem("savedCardsToStorage", JSON.stringify(filteredCards));
   }
-
-  updateIdea() {
+​
+  updateIdea(id) {
+    var savedCardsFromStorage = JSON.parse(localStorage.getItem("savedCardsToStorage"));
+    savedCardsFromStorage.forEach((item, i) => {
+      if (item.id === id) {
+        item.star = !item.star;
+      }
+    });
+    localStorage.setItem("savedCardsToStorage", JSON.stringify(savedCardsFromStorage));
   }
+​
 }
-
-
-
-// 1. An `idea.js` file that contains an `Idea` class.
-//   * `Idea` methods must include, but are not limited to:
-//     1. `constructor`
-//     2. `saveToStorage` (should only have one job which is to save the instance to storage)
-//     3. `deleteFromStorage`
-//     4. `updateIdea` (should be able to update the idea’s title, body, or starred state)
-
-// //  idea has an id, title, body, and star.
-// The id should be a unique identifier. (Note: generating a random number does NOT guarantee it will be unique)
-// title and body are strings
