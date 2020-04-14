12c5d241445fe051c54d2c1b3da09b0846b2f195
Leigh Larson <leighlarson4@gmail.com>
2020-04-14 12:35:14 -0600

    spacing

--- src/idea.js
+++ src/idea.js
@@ -9,20 +9,17 @@ class Idea {
   saveToStorage(savedCards) {
     localStorage.setItem("savedCardsToStorage", JSON.stringify(savedCards));
   }
-
-
 ​
   saveToStorage(savedCards) {
     localStorage.setItem("savedCardsToStorage", JSON.stringify(savedCards));
   }
 ​
-
   deleteFromStorage(id) {
     var savedCardsFromStorage = JSON.parse(localStorage.getItem("savedCardsToStorage"));
     var filteredCards = savedCardsFromStorage.filter(function(idea) {
       return idea.id != id;
-    } )
-  localStorage.setItem("savedCardsToStorage", JSON.stringify(filteredCards));
+    })
+    localStorage.setItem("savedCardsToStorage", JSON.stringify(filteredCards));
   }
 
   updateIdea(id) {
@@ -30,9 +27,9 @@ class Idea {
     savedCardsFromStorage.forEach((item, i) => {
     if (item.id === id) {
       item.star = !item.star;
-    }
+      }
     });
   localStorage.setItem("savedCardsToStorage", JSON.stringify(savedCardsFromStorage));
   }
-  
+
 }
